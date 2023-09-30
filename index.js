38be4a611e565d7182ca85a3b46e805348922961
Yur13_Sibusiso <90971252+Sibusiso07@users.noreply.github.com>
2023-09-30 15:14:50 +0200

    updated the code to display data when opening the app

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ import bodyParser from 'body-parser';
 
 const app = express();
 const port = 3000;
-const API_URL = "api.openweathermap.org/data/2.5/weather?q="
-const API_KEY = "&appid=API-Key"
+const API_URL = "https://api.openweathermap.org/data/2.5/weather?q="
+const API_KEY = "&appid=36bbc3a51205d7c10b12a399a701d337"
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -13,14 +13,11 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.get("/", async (req, res) => {
     try{
         const response = await axios.get(API_URL + "Sandton" + API_KEY + "&units=metric");
-        const weatherData = {
-            temp: response.data.main.temp,
-            type: response.data.weather[0].main,
-            humidity: response.data.main.humidity,
-            speed: response.data.wind.speed + "m/s",
-            gust: response.data.wind.gust + "m/s",
-        };
-        res.render("index.ejs", weatherData);
+        res.render("index.ejs", {
+                                tempData: response.data.main.temp,
+                                typeData: response.data.weather[0].main,
+                                humidityData: response.data.main.humidity,
+                                windSpeed: response.data.wind.speed + "m/s",});
     } catch (error) {
         console.error("failed to make request", error.message);
         res.render("index.ejs", {error: error.message});
@@ -31,14 +28,12 @@ app.get("/result", async (req, res) => {
     const city = req.body.id;
     try{
         const response = await axios.get(API_URL + city + API_KEY + "&units=metric");
-        const weatherData = {
-            temp: response.data.main.temp,
-            type: response.data.weather[0].main,
-            humidity: response.data.main.humidity,
-            speed: response.data.wind.speed + "m/s",
-            gust: response.data.wind.gust + "m/s",
-        };
-        res.render("index.ejs", weatherData);
+        res.render("index.ejs", {
+                                tempData: response.data.main.temp,
+                                typeData: response.data.weather[0].main,
+                                humidityData: response.data.main.humidity,
+                                windSpeed: response.data.wind.speed + "m/s",
+                                location: city,});
     } catch (error) {
         console.error("failed to make request", error.message);
         res.render("index.ejs", {error: error.message});
